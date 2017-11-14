792559d0ecc67844f8364967ff20e71bd0481796
Holly Deng <holly199975@gmail.com>
2017-11-14 14:14:06 +0800

    Numbers

---
+++ src/com/fju/mid/Numbers.java
@@ -0,0 +1,15 @@
+package com.fju.mid;
+
+import java.util.Scanner;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter a number");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		int age = Integer.parseInt(line);
+			
+	}
+
+}
