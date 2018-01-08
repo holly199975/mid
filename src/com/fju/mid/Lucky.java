dea80d75883b889c808c9a0bf1f86d411304a7af
Holly Deng <holly199975@gmail.com>
2018-01-08 18:41:09 +0800

    第六題

--- src/com/fju/mid/Lucky.java
+++ src/com/fju/mid/Lucky.java
@@ -1,10 +1,16 @@
 package com.fju.mid;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lucky {
 
 	public static void main(String[] args) {
+		System.out.println("Please enter a number:");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		int number = Integer.parseInt(line);
+		
 		Random r = new Random();
 		int n1 = r.nextInt(10)+1;
 		int n2 = r.nextInt(10)+1;
@@ -13,6 +19,23 @@ public class Lucky {
 		int n5 = r.nextInt(10)+1;
 		int n6 = r.nextInt(10)+1;
 		System.out.println(n1+" "+n2+" "+n3+" "+n4+" "+n5+" "+n6);
+		
+		if(n1 == number) {
+			System.out.println("Yes");
+		}else if(n2 == number) {
+			System.out.println("Yes");
+		}else if(n3 == number) {
+			System.out.println("Yes");
+		}else if(n4 == number) {
+			System.out.println("Yes");
+		}else if(n5 == number) {
+			System.out.println("Yes");
+		}else if(n6 == number) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+		
 	}
 
 }
